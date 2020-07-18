17a254751da1b8e9b4bb1fde505a2cd8c22aa960
EpiceneDev <48662221+EpiceneDev@users.noreply.github.com>
2020-07-18 11:29:46 -0700

    mvp met

--- hashtables/ex2/ex2.py
+++ hashtables/ex2/ex2.py
@@ -9,6 +9,18 @@ def reconstruct_trip(tickets, length):
     """
     YOUR CODE HERE
     """
-    # Your code here
+    ht = {}
+    route = [None] * length
+
+    for ticket in range(length):
+        if tickets[ticket].source == "NONE":
+            route[0] = tickets[ticket].destination
+
+        ht[tickets[ticket].source] = tickets[ticket].destination
+
+    for ticket in range(length):
+        if route[ticket-1]:
+            route[ticket] = ht[route[ticket-1]]
+            
 
     return route

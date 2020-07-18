0a629e915ad25460c374252ac6c3d14beda3b890
EpiceneDev <48662221+EpiceneDev@users.noreply.github.com>
2020-07-18 11:47:01 -0700

    fixed minor issues

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -1,24 +1,45 @@
+
 def has_negatives(a):
     """
     YOUR CODE HERE
     """
-    pos = []
-    neg = []
-    results = {}
-    #create new lists with positive and negative 
-    #iterate thru the lists comparing
-    # or
-    #check each number to be equal and opposite
-    #put the number (no sign) into results
-    for num in range(len(a)):
-        if num < 0:
-            neg.append(num)
-        elif num >= 0:
-            pos.append(num)
-        
+    # ht = {}
+    # #create new lists with positive and negative 
+    # #iterate thru the lists comparing
+    # # or
+    # #check each number to be equal and opposite
+    # #put the number (no sign) into results
+    # for num in a:
+    #     if num >= 0:
+    #         ht[num] = True
     
-    return result
+    # numlist = []
+
+    # for num in a:
+    #     if num < 0 and ht[abs(num)]:
+    #         numlist.append(abs(num))
+        
+
+    #     print(numlist)
+    
+    # return numlist
+    h = {}
+    r = []
+    for i in a:
+        if i >= 0:
+            h[i] = True
+        # print(h)
+    for i in a:
+        if i < 0 and abs(i) in h:
+            r.append(-i)
+    print(r)
+    return r
+   
+
 
 
 if __name__ == "__main__":
-    print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
+    # result = has_negatives([1,2,3,-4])
+    print(has_negatives([1,2,3,-4]))
+    # print(has_negatives([-1,-2,1,2,3,4,-4]))
+
